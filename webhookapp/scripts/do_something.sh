502bcf78ea785bb1425f9ecc977678948f58f7ab
Gogs <gogs@fake.local>
2020-11-11 15:07:34 +0000

    update: added clone and pull

--- webhookapp/scripts/do_something.sh
+++ webhookapp/scripts/do_something.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "hi there"
+CURRDIR=`pwd`
+WORKDIR="scripts"
+REPO="<name of repo to be backed up>"
+cd "${CURRDIR}/${WORKDIR}"
+
+if [ ! -d  $REPO ]
+then
+    echo " :: ${REPO} repository not found, checking it out ..."
+    git clone http://<API TOKEN FROM GOGS>@<DOCKER SERVER>:3333/<USER>/${REPO}.git
+else 
+    cd $REPO
+    git pull http://<API TOKEN FROM GOGS>@<DOCKER SERVER>:3333/<USER>/${REPO}.git
+fi
